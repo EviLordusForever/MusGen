cf8e2ea3de122c85220648a75705bdefe5d6e995
EviLordus <devil.mining.999@gmail.com>
2022-07-14 21:41:13 +0300

    Implemented GraficsMaker, FindFrequncy()

---
+++ ELFVoiceChanger/Core/GraficsMaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELFVoiceChanger.Core
+{
+	public static class GraficsMaker
+	{
+		public static void MakeGrafic(float[] a, float yScale)
+		{
+			Bitmap bmp = new Bitmap(a.Length, 1080);
+			Graphics gr = Graphics.FromImage(bmp);
+			gr.Clear(Color.White);
+			Pen pen = Pens.Black;
+
+			for (int i = 0; i < a.Length; i++)
+				gr.DrawLine(pen, i, 0, i, a[i] * yScale);
+
+			Disk.SaveImage(bmp, Disk.programFiles + "grafic");
+		}
+	}
+}
