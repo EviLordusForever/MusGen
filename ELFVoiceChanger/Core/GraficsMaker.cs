e1cd78d246cbc06e2a2c336195a1dabbeef192e8
Trigger Devil <devil.mining.999@gmail.com>
2022-07-28 18:50:16 +0300

    MakeGraficPlus() changes #2

--- ELFVoiceChanger/Core/GraficsMaker.cs
+++ ELFVoiceChanger/Core/GraficsMaker.cs
@@ -30,16 +30,22 @@ namespace ELFVoiceChanger.Core
 			double yScale = 1080 / a.Max();
 			double xScale = 1920 / a.Length;
 
-			Pen pen = new Pen(Color.Black, Convert.ToInt32(xScale + 1));
+			Pen pen = new Pen(Color.Black, Convert.ToInt32(xScale) + 1);
 			Pen redPen = new Pen(Color.Red, 3);
-			Pen greenPen = new Pen(Color.Green, 5);
+			Pen greenPen = new Pen(Color.Green, 7);
+			int buffer = 0;
 
 			for (int i = 0; i < a.Length; i++)
-				gr.DrawLine(pen, Convert.ToInt32(i * xScale), 0, Convert.ToInt32(i * xScale), Convert.ToInt32(a[i] * yScale));
+			{
+				if (a[i] > 0)
+					buffer = Convert.ToInt32(a[i] * yScale);
 
-			gr.DrawLine(redPen, redLine1, 0, redLine1, 1080);
-			gr.DrawLine(redPen, redLine2, 0, redLine2, 1080);
-			gr.DrawLine(greenPen, greenLine, 0, greenLine, 1080);
+				gr.DrawLine(pen, Convert.ToInt32(i * xScale), 0, Convert.ToInt32(i * xScale), buffer);
+			}
+
+			gr.DrawLine(redPen, Convert.ToInt32(redLine1 * xScale), 0, Convert.ToInt32(redLine1 * xScale), 1080);
+			gr.DrawLine(redPen, Convert.ToInt32(redLine2 * xScale), 0, Convert.ToInt32(redLine2 * xScale), 1080);
+			gr.DrawLine(greenPen, Convert.ToInt32(greenLine * xScale), 0, Convert.ToInt32(greenLine * xScale), 1080);
 
 			Disk.SaveImage(bmp, Disk.programFiles + "Grafics\\g_" + name + ".bmp");
 		}
--- ELFVoiceChanger/Voice/PeriodFinder.cs
+++ ELFVoiceChanger/Voice/PeriodFinder.cs
@@ -51,32 +51,31 @@ namespace ELFVoiceChanger.Voice
 		public static double FindPeriod_WithAnimation(Wav wav, int start, int end, int n)
 		{
 			//end - start MORE than maxPeriod
-			int t = 0;
-			float[] mismatches = new float[maxPeriod - minPeriod];
+			float[] mismatches = new float[maxPeriod];
 
 			double actualPeriod = 1;
 			double minMismatch = 1;
 
 			int delta = ((end - start) - maxPeriod) / points;
 
-			for (double perioud = minPeriod; perioud < maxPeriod; perioud *= 1.025)
+			for (double period = minPeriod; period < maxPeriod; period *= 1.025)
 			{
-				double mismatch = 0;
+				double mismatch = 0;    
 
 				for (int sample = start; sample < end - maxPeriod; sample += delta)
 				{
-						mismatch += Math.Abs(wav.L[sample] - wav.L[sample + (int)perioud]);
+						mismatch += Math.Abs(wav.L[sample] - wav.L[sample + (int)period]);
 						if (wav.channels == 2) //Is this necessary?
-							mismatch += Math.Abs(wav.R[sample] - wav.R[sample + (int)perioud]);
+							mismatch += Math.Abs(wav.R[sample] - wav.R[sample + (int)period]);
 				}
 				mismatch /= points;
 
-				mismatches[t++] = (float)mismatch;
+				mismatches[(int)period] = (float)mismatch;
 
 				if (mismatch < minMismatch)
 				{
 					minMismatch = mismatch;
-					actualPeriod = perioud;
+					actualPeriod = period;
 				}
 			}
 
